0df591bb789525491dd796b31b914a55d1824662
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2019-11-28 20:25:54 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll					long long
+#define vi					vector<int>
+#define vii					vector<vi>
+#define pii					pair<int, int> pii;
+#define pll					pair<ll, ll>
+#define si					set<int>
+#define F						first
+#define S						second
+#define pb					push_back
+#define tc(t)				int t; cin >> t; while(t--)
+#define _					ios_base::sync_with_stdio(false); cin.tie(NULL); 
+const ll MOD = 1e9 + 7;
+const ll INF = 1<<29;
+
+int main() {_
+	tc(t) {
+		
+	}
+
+}
