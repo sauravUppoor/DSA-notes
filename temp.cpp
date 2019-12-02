e8a94c515c75d0099412efd8ca00dd50c613f811
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2019-12-02 12:20:34 +0530

    Add new def ALL(s)

--- temp.cpp
+++ temp.cpp
@@ -5,13 +5,14 @@ using namespace std;
 #define ll					long long
 #define vi					vector<int>
 #define vii					vector<vi>
+#define ALL(s)					(s).begin(),(s).end()
 #define pii					pair<int, int> pii;
 #define pll					pair<ll, ll>
 #define si					set<int>
-#define F						first
-#define S						second
+#define F					first
+#define S					second
 #define pb					push_back
-#define tc(t)				int t; cin >> t; while(t--)
+#define tc(t)					int t; cin >> t; while(t--)
 #define _					ios_base::sync_with_stdio(false); cin.tie(NULL); 
 const ll MOD = 1e9 + 7;
 const ll INF = 1<<29;
