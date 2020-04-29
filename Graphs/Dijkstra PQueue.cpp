52abfa7ffc787d9da2c4916f726050f6bc14dd17
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2020-04-29 19:04:31 +0530

    Add snippet for path restoration

--- Graphs/Dijkstra PQueue.cpp
+++ Graphs/Dijkstra PQueue.cpp
@@ -1,13 +1,29 @@
-int distance[MAXN]; //Dsitance of node i from source node
+int distance[MAXN], pred[MAXN]; //Dsitance of node i from source node and predecessor of node i.
 bool vis[MAXN];
-priority_queue<pair<int, int>> q; // pair of -distance and node. Used negative of distance so as to get smallest value from max heap first.
-
+priority_queue<pii, vector<pii>, greater<pii>> pq; // pair of distance and node. This pq acta as Min Heap
 for (int i = 1; i <= n; i++) distance[i] = INF;
 
+void pathRestore(int s, int t) { // restores path, here s = source t= sink
+	vi path;
+	map<int,int> m;
+
+	for(int i = s; i != t; i = pred[i]) {
+		path.pb(i);
+		m[i]++;
+		if(m[i] == 2) { // This is to check incase there's no path possible ie we reach same node again.
+			cout << "-1";
+			return;
+		}
+	}
+	path.pb(1);	
+	reverse(ALL(path));
+	for(auto x: path) cout << x << " ";
+}
+
 distance[s] = 0; // starting node s
-q.push({0, s});
-while (!q.empty()) {
-    int a = q.top().second; q.pop();
+pq.push({0, s});
+while (!pq.empty()) {
+    int a = pq.top().second; pq.pop();
     
     if (vis[a]) continue;
     vis[a] = 1;
@@ -20,6 +36,9 @@ while (!q.empty()) {
             q.push({-distance[b], b});
         }
     }
+    
+    pathRestore(s, t);
 }
 
 // https://www.spoj.com/problems/HIGHWAYS/
+// https://codeforces.com/contest/20/problem/C
