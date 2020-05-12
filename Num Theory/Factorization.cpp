a218e69aa37db654f2b107bffd3285f4757148ba
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2020-05-12 16:44:02 +0530

    Add sieve approach for prime factorization

--- Num Theory/Factorization.cpp
+++ Num Theory/Factorization.cpp
@@ -1,3 +1,4 @@
+//Brute force:
 
 void factorize(int n) {
 	for(int p = 2; p <= n; ++p) {
@@ -14,6 +15,11 @@ void factorize(int n) {
 	
 	assert(n == 1);
 }
+
+//________________________________________________________________
+//LogN approach:
+//Better if you want to keep count of particular prime factors occuring in factorization
+//Less memory space
 
 void factorize_efficient(int n) {
 	for(int p = 2; p*p < n; ++p) {
@@ -38,4 +44,43 @@ void factorize_efficient(int n) {
 int main() {_
 	factorize(30);
 	factorize_efficient(90);
+}
+
+//________________________________________________________
+
+//Sieve approach:
+void sieve() {
+	memset(primes, -1, sizeof(primes));
+
+	for(int i = 2; i*i <= MAXN; ++i) {
+		if(primes[i] == -1) {
+			for(int j = i*i; j <= MAXN; j += i)
+				primes[j] = i;
+
+		}
+	}
+}
+
+void factorize_sieve(int n) {
+	vi factors;
+	
+	while(n > 1) {
+		if(primes[n] == -1) {
+			factors.pb(n);
+			n = 1;
+		}
+		else {
+			factors.pb(primes[n]);
+			n /= primes[n];
+		}
+	}
+	for(auto x: factors) cout << x << " ";
+	cout << "\n";
+}
+
+void solve() {
+	int n;
+	cin >> n;
+
+	factorize_sieve(n);
 }
