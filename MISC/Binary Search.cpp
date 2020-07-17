5c3f0ff462c08da788245e5101d21edabb5c4fc1
saurav <sauravUppoor@users.noreply.github.com>
2020-07-17 12:55:15 +0530

    BS update

--- MISC/Binary Search.cpp
+++ MISC/Binary Search.cpp
@@ -2,16 +2,16 @@
 
 // 1. Finding lower/upper bounds in a range [l..r]
 
-int bs(int l, in r) {
+int bs(int l, int r, int x) {
 	// l, r are the values in range answer exists [l...r]
 	while(l < r) {
 		int mid = (l+r)/2;
 
-		// for upper bound
+		// for Lower bound
 		if(good(mid)) l = mid;
 		else r = mid - 1;
 
-		//for lower bound
+		//for Upper bound
 		if(good(mid)) r = mid;
 		else l = mid + 1
 	}
