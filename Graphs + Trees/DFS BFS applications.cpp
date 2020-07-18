dbc02e8c3be53f4d3ebf321925dd754a8e68226b
saurav <sauravUppoor@users.noreply.github.com>
2020-07-18 13:45:10 +0530

    Add Topological Sort

--- Graphs + Trees/DFS BFS applications.cpp
+++ Graphs + Trees/DFS BFS applications.cpp
@@ -20,3 +20,44 @@ int dfs(int u) {
 
 ___________________________________________________________________
 
+Kahn's algorithm for Topological Sort - O(V + E)
+
+vi adj[MAXN];
+vi res;
+int in[MAXN];
+
+void kahn(int n) {
+	queue<int> q;
+	// priority_queue<int, vi, greater<int>> q; For lexicographically odered res
+	F0R(i,n) if(in[i] == 0) q.push(i);
+
+	while(!q.empty()) {
+		int node = q.front();
+		q.pop();
+		res.pb(node+1);
+		for(auto x: adj[node]) {
+			in[x]--;
+			if(in[x] == 0) q.push(x);
+		}
+	}
+
+	if(res.size() == n) for(auto x: res) cout << x << ' ';
+	else cout << "Sandro fails.";
+}
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	F0R(i,m) {
+		int u,v;
+		cin >> u >> v;
+		u--, v--;
+		adj[u].pb(v);
+		in[v]++;
+	}
+
+	kahn(n);
+}
+
+___________________________________________________________________
