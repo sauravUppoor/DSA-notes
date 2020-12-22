b69a21bc6b5292517530ddcef217b347c4d3b3ae
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2020-12-22 22:46:34 +0530

    Update Euler Toitient Function.cpp

--- Num Theory/Euler Toitient Function.cpp
+++ Num Theory/Euler Toitient Function.cpp
@@ -1,5 +1,5 @@
 /*  Euler Toitient Function
-    phi(x) - gives the count of numbers below x which are coprime with x
+    phi(x) - gives the count of numbers which are coprime with x in the range [1,x]
     phi(x) is multiplicative i.e. phi(x*y) = phi(x) * phi(y)
     
     General formula for phi(n)
@@ -53,3 +53,37 @@ void sieve() {
 	}
 }
     
+// -------------------------------------------------------------
+
+/* Modified ETF
+   f(p,r) - gives count of numbers which are coprime with p in the range [1,r]
+   f(p,r) = r - g(p,r)
+   where g(p,r) is the count of numbers which are not coprime with p
+   
+   We can find g(p,r) using phi_2(p,r,v) where v is a vector containing prime factors of p.
+   We use the principle of Inclusion and Exclusion to find the value.
+   
+   Eg - v = {p1,p2,p3}
+   Then g(p,r) = (r/p1) + (r/p2) + (r/p3) - (r/p1p2) - (r/p2p3) - (r/p3p1) + (r/p1p2p3)
+   Complexity -
+   For a given number p, there are log10(p) primes possible at max,
+   Thus generating subsets take O(N*2^N) = O(2^N) where N = log10(p)
+*/
+
+int phi_2(int p, int r, vi v) {
+	int sz = v.size();
+	int phi = 0;
+	for (int ii = 1; ii < (1 << sz); ii++) {
+		vi select;
+		int p = 1;
+		for (int j = 0; j < sz; j++) {
+			if (ii & (1 << j)) select.pb(v[j]), p *= v[j];
+		}
+		// D1(select.size());
+		if (select.size() % 2 == 0 && select.size() > 0) {
+			phi -= (r / p);
+		}
+		else phi += (r / p);
+	}
+	return phi;
+}
