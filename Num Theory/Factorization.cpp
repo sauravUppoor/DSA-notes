d4127cf9f224b9aaa6832b13dfe80be8e9c59fff
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2020-12-22 12:09:37 +0530

    Update and rename Prime Factorization.cpp to Factorization.cpp

--- Num Theory/Factorization.cpp
+++ Num Theory/Factorization.cpp
@@ -54,3 +54,21 @@ vector<pii> factorize(int n) {
 	return f;
 }
 
+// -------------------------------------------------------------------------------------------------
+
+/*  Get all the divisors of a number
+    Complexity - O(sqrt(n))
+*/
+
+vi divisors(int n) {
+	vi res;
+	for (int i = 1; i * i <= n; i++) {
+		if (n % i == 0) {
+			res.pb(i);
+			res.pb(n / i);
+		}
+	}
+	sort(ALL(res));
+	return {res.begin(), unique(ALL(res))};
+}
+
