6b0b17eb7ff04fa1eaffe94359ee28e0b1797ef3
sauravUppoor <sauravsuppoor@gmail.com>
2021-08-12 10:58:39 +0530

    Reorder folders

---
+++ MISC/template.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define ALL(s) (s).begin(),(s).end()
+#define ALLn(s,n) s,s+n
+#define F first
+#define S second
+#define pb push_back
+#define tc(t) int t; cin >> t; while(t--)
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define D1(x) { cerr << " [" << #x << ": " << x << "]\n"; }
+#define D2(x) { cerr << " [" << #x << ": "; for(auto it:x) cerr << it << " "; cerr << "]\n"; }
+
+const ll MOD = 1e9 + 7;
+const ll MAXN = 1e6 + 7;
+const ll INF = INT_MAX;
+
+void solve() {
+	
+}
+
+int32_t main() {_
+	//auto t1 = std::chrono::high_resolution_clock::now();
+	tc(t)
+		solve();
+	//Running time check
+	//auto t2 = chrono::high_resolution_clock::now();
+	//auto duration = chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+	//cout << duration <<" ms";
+}
+
+
